cd88b0183504f5cd1ac29e22f319dddc12a049e7
Sergio Jr <sergio23jr@gmail.com>
2019-04-25 18:27:36 -0500

    uploading images to firebase now need ot retrieve them

--- login.js
+++ login.js
@@ -8,22 +8,88 @@ var config = {
 };
 firebase.initializeApp(config);
 
-$("#signin").on("click", () => {
-    const email = $("#username").val();
-    const pass = $("#password").val();
-    const auth = firebase.auth();
-    $(".errorText").empty()
-    //If statement to validate user input
-    if (email.includes("@") && email.includes(".") && pass.length > 5) {
-        console.log("valid")
-        //Sign in
-        const promise = auth.signInWithEmailAndPassword(email, pass);
-        //If error, log it to the console
-        promise.catch(e => $(".errorText").text("Please enter a valid email/password"));
-        //TODO: If error make it modolo instead of console logging it
-        // clearInputForms();
-        //If the validation wasn't correct
-    } else {
-        console.log("invalid")
-    }
+const auth = firebase.auth();
+const db = firebase.firestore();
+const database = firebase.database();
+
+// listens for auth status changes
+auth.onAuthStateChanged(user => {
+    // console.log(user);
 })
+
+//logout method
+const logout = document.querySelector('#logout');
+logout.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log("signedout")
+    })
+})
+
+// login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = loginForm["login-email"].value;
+    const password = loginForm["login-password"].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        // console.log(cred.user)
+        loginForm.reset();
+    })
+})
+
+
+// uploading images
+const uploader = document.getElementById('uploader');
+const fileButton = document.getElementById('fileButton');
+
+fileButton.addEventListener('change', (e) => {
+    //Get file
+    let file = e.target.files[0];
+
+    //create storage ref
+    let storageRef = firebase.storage().ref('/events/' + file.name);
+
+    //upload the file
+    let task = storageRef.put(file)
+
+
+
+    //update progress bar
+    task.on('state_changed',
+
+        function progress(snapshot) {
+            let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            uploader.value = percentage
+        },
+        function error(err) {
+            console.log(err)
+
+        },
+        function complete(snapshot) {
+            // empty out the file uploaded
+
+            //get url for pic
+            task.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+                console.log('File available at', downloadURL);
+                // send the url to database
+
+
+            });
+
+
+
+
+        }
+
+    )
+})
+
+//getdata
+db.collection('/events/').get().then(snapshot => {
+    // console.log(snapshot.docs[0]._document.proto.name)
+    console.log(snapshot)
+})
+
+//delete images from storage
